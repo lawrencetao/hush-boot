028ecfdcad6e0ba2604e116f5642678251d2091e
lawrencetao <taobao3809@163.com>
2017-12-28 13:00:05 +0800

    增加@EnableDiscoveryClient注解。

--- hush-turbine/src/main/java/com/lawrence/hush/TurbineApplication.java
+++ hush-turbine/src/main/java/com/lawrence/hush/TurbineApplication.java
@@ -2,9 +2,11 @@ package com.lawrence.hush;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableTurbine
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TurbineApplication {
 
