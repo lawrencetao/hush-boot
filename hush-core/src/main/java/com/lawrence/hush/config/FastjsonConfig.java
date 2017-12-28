8d8063340faf13d824f3644384975509e6d2570d
lawrencetao <taobao3809@163.com>
2017-12-28 10:26:36 +0800

    加入hystrix-dashboard和turbine聚合。

---
+++ hush-core/src/main/java/com/lawrence/hush/config/FastjsonConfig.java
@@ -0,0 +1,79 @@
+package com.lawrence.hush.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * fastjson配置类
+ */
+@Configuration
+@ConditionalOnClass(value = {com.alibaba.fastjson.JSON.class, com.alibaba.fastjson.JSONObject.class})
+@ConditionalOnMissingBean(FastJsonHttpMessageConverter4.class)
+public class FastjsonConfig {
+
+    /**
+     * HttpMessage转换器
+     */
+    @Bean
+    public FastJsonHttpMessageConverter4 fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter4 converter = new FastJsonHttpMessageConverter4();
+        converter.setFastJsonConfig(fastjsonConfig());
+        converter.setSupportedMediaTypes(getSupportedMediaType());
+
+        return converter;
+    }
+
+    /**
+     * fastjson的配置
+     */
+    public FastJsonConfig fastjsonConfig() {
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+
+        // 格式化json, null值不输出
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+
+        // 格式化日期
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // null值替换为""
+        /*ValueFilter valueFilter = new ValueFilter() {
+            public Object process(Object o, String s, Object o1) {
+                if (null == o1) {
+                    o1 = "";
+                }
+                return o1;
+            }
+        };
+        fastJsonConfig.setSerializeFilters(valueFilter);*/
+
+        // 设置编码
+        fastJsonConfig.setCharset(Charset.forName("UTF-8"));
+
+        return fastJsonConfig;
+    }
+
+    /**
+     * 支持的mediaType类型
+     */
+    public List<MediaType> getSupportedMediaType() {
+        ArrayList<MediaType> mediaTypes = new ArrayList<>();
+
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        mediaTypes.add(MediaType.APPLICATION_JSON);
+        mediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+
+        return mediaTypes;
+    }
+
+}
--- hush-webapp/src/main/java/com/lawrence/hush/WebappApplication.java
+++ hush-webapp/src/main/java/com/lawrence/hush/WebappApplication.java
@@ -2,6 +2,7 @@ package com.lawrence.hush;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
