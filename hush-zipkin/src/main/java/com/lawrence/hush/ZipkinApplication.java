5b2847afc04b711e1e6c6f9c8b19eef67365cf62
lawrencetao <taobao3809@163.com>
2018-01-05 14:16:58 +0800

    hush-zipkin添加详细注释说明。

--- hush-zipkin/src/main/java/com/lawrence/hush/ZipkinApplication.java
+++ hush-zipkin/src/main/java/com/lawrence/hush/ZipkinApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
+/**
+ * zipkin链路追踪, 日志中心, 注册到eureka提供服务
+ */
 @EnableZipkinStreamServer
 @EnableDiscoveryClient
 @SpringBootApplication
