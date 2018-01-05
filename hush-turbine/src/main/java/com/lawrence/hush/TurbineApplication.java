0894b40ff250d2408059e2f93497de41979f0c1f
lawrencetao <taobao3809@163.com>
2018-01-05 11:36:13 +0800

    hush-turbine添加详细注释说明。

--- hush-turbine/src/main/java/com/lawrence/hush/TurbineApplication.java
+++ hush-turbine/src/main/java/com/lawrence/hush/TurbineApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * hystrix-dashboard和turbine聚合, 监控中心, 注册到eureka提供服务
+ */
 @EnableTurbine
 @EnableHystrixDashboard
 @EnableDiscoveryClient
