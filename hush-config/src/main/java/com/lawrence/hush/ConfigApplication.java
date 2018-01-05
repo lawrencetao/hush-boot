3bb25592b624d73d3c4d9f2e7289f3c51ecf00ad
lawrencetao <taobao3809@163.com>
2018-01-05 11:29:35 +0800

    hush-config添加详细注释说明。

--- hush-config/src/main/java/com/lawrence/hush/ConfigApplication.java
+++ hush-config/src/main/java/com/lawrence/hush/ConfigApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * config服务端, 配置中心, 注册到eureka提供服务
+ */
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
