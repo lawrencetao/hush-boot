cb343d0a1229df4286ba72f8a27c07a45834b6ad
lawrencetao <taobao3809@163.com>
2018-01-10 15:44:01 +0800

    hush-admin添加处理，只在初始接到ContextRefreshedEvent时进行操作。

--- hush-admin/src/main/java/com/lawrence/hush/listener/ContextRefreshedListener.java
+++ hush-admin/src/main/java/com/lawrence/hush/listener/ContextRefreshedListener.java
@@ -1,15 +1,20 @@
 package com.lawrence.hush.listener;
 
 import com.lawrence.hush.util.LogUtil;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.ContextLoader;
 
 /**
  * spring容器监听器
  */
 @Component
 public class ContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    // 容器是否已加载
+    private static boolean started = false;
 
     /**
      * 容器加载完成后执行制定操作
@@ -18,9 +23,12 @@ public class ContextRefreshedListener implements ApplicationListener<ContextRefr
      */
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (!started) {
+            started = true;
 
-        LogUtil.info(getClass(), "ContextRefreshedEvent监听事件调用, Hush-boot: Spring容器加载完成");
+            LogUtil.info(getClass(), "ContextRefreshedEvent监听事件调用, Hush-boot: Spring容器加载完成");
 
+        }
     }
 
 }
